e94b0a9a0959a317e915be8032ad1fc9505c586e
VijinKV <vijinnewman@gmail.com>
2023-04-29 10:47:54 +0530

    Intermediate commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Intelligent Notes
+
+This Plugin will have everything that will make your note-making life easier
+
+## Usage
+
+<!-- TODO: Describe usage -->
+
+<!-- ignore-after -->
--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,13 +1,48 @@
 import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
+import { useAPIEventListener, AppEvents } from "@remnote/plugin-sdk";
 
 async function onActivate(plugin: ReactRNPlugin) {
+
+  console.log("Hello from my RemNote plugin!");
+  await plugin.app.registerCSS("hide-hello-css", `
+    .rem-text span[data-key="hello"] {
+      display: none;
+    }
+  `);
+
+  plugin.event.addListener(AppEvents.RemChanged, undefined, async (remId) => {
+    console.log("useAPIEventListener");
+    const rem = await plugin.rem.findOne(remId);
+    const text = rem?.text;
+    if (!text) return;
+    const updatedText = text.map((richText) => {
+      console.log("richtext");
+      if (typeof richText === "string") {
+        console.log("richtext woo its string");
+        const words = richText.split(" ");
+        const newText = words.map((word) => {
+          console.log("new word here");
+          if (word.toLowerCase() === "hello") {
+            return { text: word, i: "hello", "data-key": "hello" };
+          }
+          return word;
+        });
+        
+        return newText.join("*");
+      }
+      return richText;
+    });
+  
+    await rem.setText(updatedText);
+  });
+
   // Register settings
   await plugin.settings.registerStringSetting({
     id: 'name',
-    title: 'What is your Name?',
-    defaultValue: 'Bob',
+    title: 'What is your Ohhh fuckk ukk?',
+    defaultValue: 'Fuck',
   });
 
   await plugin.settings.registerBooleanSetting({
@@ -32,12 +67,14 @@ async function onActivate(plugin: ReactRNPlugin) {
   });
 
   // Show a toast notification to the user.
-  await plugin.app.toast("I'm a toast!");
+  await plugin.app.toast("Okkkaaasssss!");
 
   // Register a sidebar widget.
   await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
     dimensions: { height: 'auto', width: '100%' },
   });
+
+
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
